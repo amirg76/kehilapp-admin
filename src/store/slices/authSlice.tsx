47120f12ca86e4ef230ecb2c515de2ed8dbdb5f8
amirg76 <amirg76@gmail.com>
2024-09-16 13:13:01 +0300

    add browser router and redux store for auth

---
+++ src/store/slices/authSlice.tsx
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: { currentUser: null, isAuthenticated: false },
+  reducers: {
+    login: (state, action) => {
+      state.currentUser = action.payload;
+      state.isAuthenticated = true;
+    },
+    logout: (state) => {
+      state.currentUser = null;
+      state.isAuthenticated = false;
+    },
+    register: (state, action) => {
+      state.currentUser = action.payload;
+      state.isAuthenticated = true;
+    },
+  },
+});
+
+export const authReducer = authSlice.reducer;
+export const authActions = authSlice.actions;
+
+export default authSlice;
