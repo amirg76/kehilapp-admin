2fe83375b8abd5d24b4e2ec4671971a9eaa26fd3
amirg76 <amirg76@gmail.com>
2024-10-07 10:41:02 +0300

    change new user inputs

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,59 +1,11 @@
-import { GridColDef } from "@mui/x-data-grid";
 import DataTable from "../../components/dataTable/DataTable";
 import "./Users.scss";
 import { useEffect, useState } from "react";
 import Add from "../../components/add/Add";
-import { userRows } from "../../data";
+import { columns } from "../../data";
 import { uploadExcelFile } from "@/features/authentication/helpers/uploadExcelFile";
 import { sentFileSuccess } from "@/features/authentication/helpers/sentFileSuccess";
 import { getUsersFromDb } from "@/features/authentication/helpers/getUsersFromDb";
-const columns: GridColDef[] = [
-  { field: "id", headerName: "ID", width: 90 },
-  {
-    field: "img",
-    headerName: "Avatar",
-    width: 100,
-    renderCell: (params) => {
-      return <img src={params.row.img || "/noavatar.png"} alt="" />;
-    },
-  },
-  {
-    field: "firstName",
-    type: "string",
-    headerName: "First name",
-    width: 150,
-  },
-  {
-    field: "lastName",
-    type: "string",
-    headerName: "Last name",
-    width: 150,
-  },
-  {
-    field: "email",
-    type: "string",
-    headerName: "Email",
-    width: 200,
-  },
-  {
-    field: "phone",
-    type: "string",
-    headerName: "Phone",
-    width: 200,
-  },
-  {
-    field: "createdAt",
-    headerName: "Created At",
-    width: 200,
-    type: "string",
-  },
-  {
-    field: "verified",
-    headerName: "Verified",
-    width: 150,
-    type: "boolean",
-  },
-];
 
 const Users = () => {
   const [open, setOpen] = useState(false);
@@ -92,7 +44,7 @@ const Users = () => {
         <input type="file" accept=".xlsx, .xls" onChange={handleFileChange} />
         {fileError && <p style={{ color: "red" }}>{fileError}</p>}
       </div>
-      <DataTable slug="users" columns={columns} rows={userRows} />
+      <DataTable slug="users" columns={columns} rows={users} />
       {/* TEST THE API */}
 
       {/* {isLoading ? (
@@ -100,7 +52,13 @@ const Users = () => {
       ) : (
         <DataTable slug="users" columns={columns} rows={data} />
       )} */}
-      {open && <Add slug="user" columns={columns} setOpen={setOpen} />}
+      {open && (
+        <Add
+          slug="user"
+          columns={columns.filter((column) => column.field !== "createdAt")}
+          setOpen={setOpen}
+        />
+      )}
     </div>
   );
 };
