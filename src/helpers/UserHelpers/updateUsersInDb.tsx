7d0fd62486c98310a27a3d5e6c860fab3a23c3a7
amirg76 <amirg76@gmail.com>
2024-10-27 23:40:41 +0200

    add userUpdate to use useCustomMutation

--- src/helpers/UserHelpers/updateUsersInDb.tsx
+++ src/helpers/UserHelpers/updateUsersInDb.tsx
@@ -1,52 +1,20 @@
-import { handleRequest } from "@/services/handleRequest";
 import { UPDATE_TABLE_ITEM_URL } from "@/api/apiConstants";
-import moment from "moment";
-import { useDispatch } from "react-redux";
-import { usersActions } from "@/store/slices/usersSlice";
+import { useCustomMutation } from "@/services/useMutationService";
 
-interface User {
-  id: number;
-  [key: string]: any;
+export interface User {
+  id?: string;
+  name: string;
+  email: string;
+  // ... other user properties
 }
 interface UpdateUserData {
   id: string;
   updateData: Partial<User>;
 }
-
-const formatUserDates = (users: User[]): User[] => {
-  return users.map((user) => ({
-    ...user,
-    createdAt: moment(user.createdAt).format("DD.MM.YYYY"),
-  }));
-};
-export const updateUsersInDb = () => {
-  const dispatch = useDispatch();
-  const onUpdateUsersSuccess = (data: any): void => {
-    // const status = data?.error?.status;
-
-    // if (status !== undefined) {
-    //   updateErrorMessage(status, setErrorMessage);
-    if (data.length > 0) {
-      const formattedUsers = formatUserDates(data);
-      console.log(formattedUsers);
-      dispatch(usersActions.setUsers(formattedUsers));
-    }
-    // }
-  };
-  const updateUsersHandler = handleRequest<User[], UpdateUserData>({
+export const useUpdateUser = () => {
+  return useCustomMutation<UpdateUserData>({
     url: UPDATE_TABLE_ITEM_URL,
     method: "post",
-    handleSuccessCallback: onUpdateUsersSuccess,
-    // onLoginError
+    queryKeysToInvalidate: ["users"],
   });
-
-  const handleUpdateUsers = (itemUpdates: { id: string; updateData: {} }) => {
-    updateUsersHandler.mutateAsync(itemUpdates);
-  };
-
-  return {
-    handleUpdateUsers,
-    isLoading: updateUsersHandler.isLoading,
-    mutateAsync: updateUsersHandler.mutateAsync,
-  };
 };
